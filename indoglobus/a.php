17e9276c8a9041cdfc6ce02472da78a7fd140f5a
Meral <hi.meral@gmail.com>
2020-05-03 19:59:27 +0530

    unstaged to stage and then commit

--- indoglobus/a.php
+++ indoglobus/a.php
@@ -1,17 +1,4 @@
-- \themes\blankslate\services.php 
-- \themes\blankslate\functions.php  
-- \themes\blankslate\css\newStyle.css
-- \plugins\news-announcement-scroll
-- Modify config with --> define( 'automatic_updater_disabled', true );
-- Menu > Facility & Service > Cardiac Consultation > Modify to --> /diagnostic-services/cardiac-consultation
-- Facility & Service > ECG  >  (Get the text in notepad and repaste)
-- Blog page
-	- Create new blog post type
-	- copy all the blogs from post
-	- create blog template (same as case study) and use posttype blog
-	- remove existing Blog post page
-	- create new blog page (use blog template) and set permalink with  /blogs/  slug
-	
-	- add data in registration table
 
-
+reply akshay
+reply pdf
+espl folder
